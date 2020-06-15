9bf7ff90e4d7a70a2dd3d066e3074bd01615c8a1
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2020-06-14 20:04:13 -0400

    MVP for app, starting testing

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,18 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+export const useForm = (initialValue) => {
+
+    const [values, setValues] = useState(initialValue);
+
+    const handleChanges = e => {
+        // console.log(e.target.name, e.target.value);
+
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    return [values, handleChanges];
+}
